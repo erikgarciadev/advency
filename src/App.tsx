753851a8393da2a0484fea47b5d27117a2cfb882
Erik Garcia Tacas <erik.fisi16@gmail.com>
2021-12-05 10:16:36 -0500

    init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = function () {
+    return (
+        <div className="App">
+            <p style={{ fontWeight: 'bold', fontSize: '16px' }}>Regalos</p>
+        </div>
+    )
+}
+
+export default App
